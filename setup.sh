081bbc807643cd2e5591945428c4318c3be9bc60
Maxence Ahlouche <maxence.ahlouche@gmail.com>
2018-04-16 21:31:50 +0000

    update psycopg2, no more need to retrieve it from github

--- setup.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env sh
-
-# This script installs the python dependencies
-# Necessary because some dependencies can't be handled automatically by pip
-
-pip install -r requirements.txt
-pip install git+https://github.com/psycopg/psycopg2.git@master
